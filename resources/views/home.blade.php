3676ec72849b804968deabbc2d19cca477baa82f
shubham <shubham11mishra@gmail.com>
2023-12-21 19:42:16 -0800

    task1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task - shubham mishra</title>
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="w-3/4 mx-auto mt-5 h-16 flex justify-start items-center bg-gray-800 rounded-full shadow-md">
+        <div class="bg-gray-950 px-5 py-2 rounded-full shadow-lg ml-5 cursor-pointer">
+            <a href=""><span class="font-bold text-white">Task One</span></a>
+        </div>
+        <div class="bg-yellow-400 px-5 py-2 rounded-lg shadow-sm ml-5 cursor-pointer">
+            <a href=""><span class="font-bold text-white">Task One</span></a>
+        </div>
+        <div class="bg-yellow-400 px-5 py-2 rounded-lg shadow-sm ml-5 cursor-pointer">
+            <a href=""><span class="font-bold text-white">Task One</span></a>
+        </div>
+
+    </div>
+    <div class="w-3/4 mx-auto mt-5">
+        <div class="">
+            <form action="" method="post">
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,15 @@ use Inertia\Inertia;
 |
 */
 
+
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    // return Inertia::render('Welcome', [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => Application::VERSION,
+    //     'phpVersion' => PHP_VERSION,
+    // ]);
+    return view('home');
 });
 
 Route::get('/dashboard', function () {
